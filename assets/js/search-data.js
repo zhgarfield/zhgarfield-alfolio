d5352630e18cfa7fcb1201aa7e32c03aceda3ac4
zhgarfield <13929133+zhgarfield@users.noreply.github.com>
2026-05-02 20:19:13 +0000

    Deploying to gh-pages from @ zhgarfield/zhgarfield-alfolio@67b3cc29356d9c1851994da3c2840c495b7b007d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/zhgarfield-alfolio/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/zhgarfield-alfolio/repositories/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -50,6 +43,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/zhgarfield-alfolio/teaching/";
+          },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/zhgarfield-alfolio/repositories/";
           },
         },{id: "post-a-post-with-plotly-js",
         
