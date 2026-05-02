1592cab502b3efe3e6c0ab706b025e664e22f436
zhgarfield <13929133+zhgarfield@users.noreply.github.com>
2026-05-02 08:10:33 +0000

    Deploying to gh-pages from @ zhgarfield/zhgarfield-alfolio@cc4d0d4ce094270bd9e4b01cf2920225c5d1ce86 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://alshedivat.github.io/al-folio/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://zhgarfield.github.io//zhgarfield-alfolio/#contact">contact us</a>.'
               }
             ]
           }
